84585907fcc10b3a0e52c1564c3ca4e77d7051b5
Fereshta Yazdani <yazdani@cs.uni-bremen.de>
2016-08-03 19:31:52 +0200

    added

---
+++ img_mission/catkin_generated/setup_cached.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/yazdani/work/ros/indigo/catkin_ws/src/sar_mission/img_mission/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/yazdani/work/ros/indigo/catkin_ws/src/sar_mission/img_mission/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/lib:/opt/ros/indigo/lib/typelib:/home/yazdani/work/ros/indigo/catkin_ws/src/sar_mission/img_mission/devel/lib:/home/yazdani/work/ros/indigo/catkin_ws/src/sar_mission/img_mission/devel/lib/x86_64-linux-gnu:/home/yazdani/work/ros/indigo/catkin_ws/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/usr/local/lib:/usr/lib/jvm/java-7-openjdk-amd64/jre/lib/amd64:/usr/lib/jvm/java-7-openjdk-amd64/jre/lib/amd64/server:/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/lib:/opt/ros/indigo/lib/typelib:/home/yazdani/work/ros/indigo/catkin_ws/devel/lib:/opt/ros/indigo/lib:/home/yazdani/work/ros/indigo/bologna_ws/devel/lib:/home/yazdani/work/ros/indigo/bologna_ws/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/python2.7/dist-packages"
+export PATH="/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/bin:/home/yazdani/work/ros/indigo/catkin_ws/src/sar_mission/img_mission/devel/bin:/home/yazdani/work/ros/indigo/catkin_ws/devel/bin:/opt/ros/indigo/bin:/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/bin:/home/yazdani/work/ros/indigo/bologna_ws/devel/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/jvm/default-java/bin"
+export PKG_CONFIG_PATH="/home/yazdani/work/ros/indigo/catkin_ws/src/sar_mission/img_mission/devel/lib/pkgconfig:/home/yazdani/work/ros/indigo/catkin_ws/src/sar_mission/img_mission/devel/lib/x86_64-linux-gnu/pkgconfig:/home/yazdani/work/ros/indigo/catkin_ws/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/home/yazdani/work/ros/indigo/catkin_ws/devel/lib/pkgconfig:/opt/ros/indigo/lib/pkgconfig:/home/yazdani/work/ros/indigo/bologna_ws/devel/lib/pkgconfig:/home/yazdani/work/ros/indigo/bologna_ws/devel/lib/x86_64-linux-gnu/pkgconfig"
+export PYTHONPATH="/home/yazdani/work/ros/indigo/catkin_ws/src/sar_mission/img_mission/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/yazdani/work/ros/indigo/catkin_ws/src/sar_mission/img_mission/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_MAVEN_DEPLOYMENT_REPOSITORY="/home/yazdani/work/ros/indigo/catkin_ws/src/sar_mission/img_mission/devel/share/maven"
+export ROS_MAVEN_PATH="/home/yazdani/work/ros/indigo/catkin_ws/src/sar_mission/img_mission/devel/share/maven:$ROS_MAVEN_PATH"
+export ROS_PACKAGE_PATH="/home/yazdani/work/ros/indigo/catkin_ws/src/sar_mission/img_mission:/home/yazdani/work/ros/indigo/catkin_ws/src:/opt/ros/indigo/share:/opt/ros/indigo/stacks"
---
+++ img_mission/src/store_image.cpp
@@ -0,0 +1,99 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+#include <sstream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include "sensor_msgs/Image.h"
+#include <cstdlib>
+#include <string>
+#include "img_mission/returnString.h"
+#include <stdlib.h> 
+#include <ros/package.h>
+
+using namespace std;
+using namespace cv;
+
+
+static int indexy;
+static int indexi;
+static string kette;
+static int indexer;
+ros::Subscriber sub;
+
+
+void callback(const sensor_msgs::ImageConstPtr& msg)
+{
+  std::string path = ros::package::getPath("img_mission") + "/imgs/";
+  cv::imwrite(path+kette,cv_bridge::toCvShare(msg, "rgb8")->image);
+  cv::waitKey(30);
+}
+
+
+bool check(img_mission::returnString::Request  &req,
+	   img_mission::returnString::Response &res)
+{  
+
+  indexy = 1;
+  indexer = rand() % 10000;
+  stringstream ss;
+  ss << indexer;
+  string str = ss.str();
+  kette = "image_"+str+".jpg";
+  res.result = "image_"+str+".jpg";
+  return true;
+}
+
+int main(int argc, char **argv)
+{
+  // extern int indexy;
+  ros::init(argc, argv, "GetThatString");
+  ros::NodeHandle n;
+  ros::NodeHandle nh;
+  ros::NodeHandle n_pub;
+  ros::Rate loop_rate(2);
+
+  // ros::Publisher chatter_pub = n_pub.advertise<std_msgs::String>("img_publisher", 1000);
+  ros::ServiceServer service = n.advertiseService("store_image", check);
+  indexy = 0;
+  indexi = 0;
+  kette = "";
+  while(ros::ok())
+    {
+      
+      if(indexy == 1)
+      	{
+      	  while(indexi < 5)
+      	    {
+	      ros::Subscriber  sub = nh.subscribe("/camera/rgb/image_raw", 1, callback);
+	      
+	      //ROS_INFO_STREAM(indexi);
+	      // ROS_INFO_STREAM(indexy == 1);
+	      //    std::cout << indexy << std::endl;
+	      
+	      indexi = indexi +1;
+	      if (indexi >= 4)
+		{
+		  break;
+		}
+	      loop_rate.sleep();
+	      ros::spinOnce();
+	    }
+	  //std_msgs::String ts;
+	  
+	  
+	  // ts.data = kette;
+	  //  chatter_pub.publish(ts);
+	  // ROS_INFO_STREAM(kette);
+	  indexy = 0;
+	  indexi = 0;
+		}  
+      ros::spinOnce();
+    }
+  //ros::spin();
+
+  return 0;
+}
