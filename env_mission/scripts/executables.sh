26ca512be6038e9c1524d5d6ffb205967077b744
Fereshta Yazdani <yazdani@cs.uni-bremen.de>
2017-03-24 16:19:44 +0100

    added executables script

---
+++ env_mission/scripts/executables.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+terminator -e 'bash -c "rosrun start_mission start_all_objects& rosrun start_mission start_salient_objects& rosrun start_mission start_checking_property& rosrun start_mission start_checking_property& rosrun start_mission start_getting_object_size& rosrun start_mission start_getting_property_list& rosrun start_mission start_getting_reasoning_on_pose& rosrun start_mission start_robot_rotation"; exec bash'
