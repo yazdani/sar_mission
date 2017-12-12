0511188fb51e495a3582774c0ebefb6feb6afa0d
Fereshta Yazdani <yazdani@cs.uni-bremen.de>
2017-12-12 15:03:23 +0100

    commented collision avoidance

--- start_mission/src/prolog.lisp
+++ start_mission/src/prolog.lisp
@@ -96,19 +96,26 @@
       (adjust-map ?costmap ?object-pose ?object-name))
 
 (<- (adjust-map ?costmap ?object-pose ?object-name)
-  (semantic-map-costmap::semantic-map-objects ?all-objects)
+  ;;(semantic-map-costmap::semantic-map-objects ?all-objects)
+  (format "get-elem-pose~%")
   (lisp-fun get-elem-pose ?object-name ?pose)
-  (lisp-fun get-geom-objects ?all-objects 10 ?pose ?objects)
+  ;;(lisp-fun get-geom-objects ?all-objects 10 ?pose ?objects)
   (costmap-padding ?padding)
-  (costmap-add-function semantic-map-free-space
-                        (make-semantic-map-costmap-human
-                         ?objects :invert t :padding ?padding)
-                        ?costmap)
+  ;;(costmap-add-function semantic-map-free-space
+  ;;                      (make-semantic-map-costmap-human
+  ;;                       ?objects :invert t :padding ?padding)
+  ;;                      ?costmap)
   (costmap ?costmap)
   (instance-of gaussian-generator ?gaussian-generator-id)
   (costmap-add-function ?gaussian-generator-id
                         (make-location-cost-function ?object-pose  3.0)
-                        ?costmap)))
+                        ?costmap)
+  (costmap ?costmap)
+ (costmap-add-height-generator
+   (make-consistent-height-function ?object-name ?resulting-z)
+   ?costmap)
+))
+
 
 
 ;; (def-fact-group reasoning-costmap (desig-costmap)
